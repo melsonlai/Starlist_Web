51a1f24ea1bde83488f8066d4fb337e736a4662a
張育維 <waynehappyhome@gmail.com>
2017-05-12 21:50:56 +0800

    todoItem

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -7,11 +7,7 @@ import uuid from "uuid/v4";
 import {accomplishTodo} from 'states/todo-actions.js';
 
 import './TodoItem.css';
-import MobileTearSheet from '../../../MobileTearSheet';
 import {List, ListItem} from 'material-ui/List';
-import Divider from 'material-ui/Divider';
-import Subheader from 'material-ui/Subheader';
-import Avatar from 'material-ui/Avatar';
 import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
