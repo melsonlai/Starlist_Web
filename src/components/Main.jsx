8f51e4a8c3ecce2cb990efb60eb0a920382edcd3
Lyeuiechang <uiechanglai@gmail.com>
2017-05-15 00:48:16 +0800

    New UI

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,6 +9,13 @@ import {
 } from 'reactstrap';
 import {connect} from 'react-redux';
 
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ActionGrade from 'material-ui/svg-icons/action/grade';
+
+import Dialog from 'material-ui/Dialog';
+
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
@@ -16,6 +23,19 @@ import SettingIcon from 'material-ui/svg-icons/action/settings';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import TextField from 'material-ui/TextField';
+import {fade} from 'material-ui/utils/colorManipulator';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {
+    indigo500,indigo400,
+
+    grey900,grey800,grey700,
+
+    indigoA100, indigoA200, indigoA400,
+
+    fullWhite,
+
+
+} from 'material-ui/styles/colors';
 
 import Todo from 'components/Todo.jsx';
 import {setSearchText} from 'states/post-actions.js';
@@ -24,6 +44,39 @@ import {toggleNavbar} from 'states/main-actions.js';
 import './Main.css';
 
 injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: indigo500,
+
+    primary2Color: indigo500,
+
+    primary3Color: grey700,
+
+    accent1Color: indigoA200,
+
+    accent2Color: indigoA400,
+
+    accent3Color: indigoA100,
+
+    textColor: indigo400,
+
+    secondaryTextColor: fade(indigo400, 0.7),
+
+    alternateTextColor: '#303030',
+
+    canvasColor: '#303030',
+
+    borderColor: fade(indigo400, 0.3),
+
+    disabledColor: fade(indigo400, 0.3),
+
+    pickerHeaderColor: fade(indigo400, 0.12),
+
+    clockCircleColor: fade(indigo400, 0.12),
+  },
+});
+
 
 class Main extends React.Component {
     static propTypes = {
@@ -45,35 +98,25 @@ class Main extends React.Component {
 
     render() {
         return (
-			<MuiThemeProvider>
+			<MuiThemeProvider muiTheme={muiTheme}>
                 <div className='main'>
                     <div className='bg-faded'>
-                        <div className='container'>
-                            <Navbar color='faded' light toggleable>
-                                <NavbarToggler right onClick={this.handleNavbarToggle}/>
-                                <NavbarBrand className='text-info' href="/">Starlist</NavbarBrand>
-                                <Collapse isOpen={this.props.navbarToggle} navbar>
-                                    <div className='search ml-auto'>
-                                        <TextField className='ml-auto TextField' hintText="Search" onKeyPress={this.handleSearchKeyPress} getRef={e => this.searchEl = e}/>
-                                        {
-                                            this.props.searchText &&
-                                            <i className='navbar-text fa fa-times' onClick={this.handleClearSearch}></i>
-                                        }
-                                    </div>
-									<div>
+                        <AppBar
+                            title="Starlist"
+                            iconElementLeft={<IconButton><ActionGrade /></IconButton>}
+                            iconElementRight={
 										<IconMenu
 											iconButtonElement={<IconButton><SettingIcon /></IconButton>}
 											anchorOrigin={{horizontal: 'right', vertical: 'top'}}
 											targetOrigin={{horizontal: 'right', vertical: 'top'}}
 									    >
+                                            <MenuItem primaryText="Search"/>
 											<MenuItem primaryText="Account Settings" />
 											<MenuItem primaryText="Help" />
 											<MenuItem primaryText="Sign out" />
 									    </IconMenu>
-									</div>
-                                </Collapse>
-                            </Navbar>
-                        </div>
+                                }
+                        />
                     </div>
                     <Todo />
                     <div className='footer'>
