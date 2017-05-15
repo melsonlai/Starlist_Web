07537272e54a53538aaf910021cc73eeb183b093
Lyeuiechang <uiechanglai@gmail.com>
2017-05-15 21:18:35 +0800

    Example Messages

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -7,6 +7,7 @@ import TodoItem from 'components/TodoItem.jsx';
 import {List, ListItem} from 'material-ui/List';
 import Checkbox from "material-ui/Checkbox";
 import {Card, CardTitle} from "material-ui/Card";
+import {CardActions, CardHeader, CardText} from 'material-ui/Card';
 
 class TodoList extends React.Component {
     static propTypes = {
@@ -23,10 +24,34 @@ class TodoList extends React.Component {
         let children = (
 			<ListItem>
 				<Card>
-					<CardTitle className="d-flex justify-content-center align-items-center">
+					<CardTitle /*className="d-flex justify-content-center align-items-center"*/>
                 		<div>All todos are accomplished.<br />Anything else?</div>
 					</CardTitle>
 				</Card>
+                <br />
+                <Card>
+                    <CardHeader
+                        title="第一屆 軟實 廢文大賽"
+                        subtitle="2017 May 16"
+                        //className="d-flex justify-content-center align-items-left"
+                    />
+                </Card>
+                <br />
+                <Card>
+                    <CardHeader
+                        title="蹦蹦跳 姆咪~"
+                        subtitle="2017 June 3"
+                        //className="d-flex justify-content-center align-items-left"
+                    />
+                </Card>
+                <br />
+                <Card>
+                    <CardHeader
+                        title="Pipeline 大法好"
+                        subtitle="2017 April 30"
+                        //className="d-flex justify-content-center align-items-left"
+                    />
+                </Card>
             </ListItem>
         );
         if (todos.length) {
